b1109ded66c29b54631dfa2807d0f895f426c84c
Tanguy Rozan <tanguy@MacBook-Air-de-Tanguy.local>
2025-01-11 15:16:53 +0100

    Ajout du projet Netbeans

---
+++ TP-medev/src/main/java/com/mycompany/tp/medev/TPMedev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp.medev;
+
+/**
+ *
+ * @author tanguy
+ */
+public class TPMedev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
