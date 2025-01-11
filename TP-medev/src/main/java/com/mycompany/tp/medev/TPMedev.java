d21b3ccc09b2949c6b8e466afd04d5a8792ea60d
tanguyrz <Tanguy.Rozan@eleves.ec-nantes.fr>
2025-01-11 16:18:13 +0100

    Update TPMedev.java

--- TP-medev/src/main/java/com/mycompany/tp/medev/TPMedev.java
+++ TP-medev/src/main/java/com/mycompany/tp/medev/TPMedev.java
@@ -11,6 +11,6 @@ package com.mycompany.tp.medev;
 public class TPMedev {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worlzd!");
     }
 }
